580df6e9d598c47f30c7b3de40eb7f8184b533cf
ADMIN <phongltth1807075@fpt.edu.vn>
2019-02-25 15:15:20 +0700

    validate-form

---
+++ public/js/validate.js
@@ -0,0 +1,39 @@
+$('#register-form').validate({
+    rules: {
+        name: {
+            required: true,
+            minlength: 2,
+        },
+        phone: {
+            required: true,
+            minlength: 10,
+            maxlength: 11,
+            phone: true
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        address: {
+            required: true,
+        },
+    },
+    messages: {
+        name: {
+            required: 'Vui lòng nhập tên của bạn.',
+            minlength: 'Tên quá ngắn, vui lòng nhập ít nhất {0} ký tự',
+        },
+        phone: {
+            required: 'Vui lòng nhập số điện thoại của bạn.',
+            minlength: 'Số điện thoại quá ngắn, vui lòng nhập ít nhất {0} ký tự',
+            maxlength: 'Số điện thoại quá dài, vui lòng nhập nhiều nhất {0} ký tự',
+        },
+        email: {
+            required: 'Vui lòng nhập email của bạn.',
+            email: 'Vui lòng nhập email đúng định dạng'
+        },
+        address: {
+            required: 'Vui lòng nhập địa chỉ của bạn.',
+        },
+    }
+});
